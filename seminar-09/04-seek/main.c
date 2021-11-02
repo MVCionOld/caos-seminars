6554bc0070f5f2a827c6047e7a0f896c9cf2a600
MVCionOld <tsionmichel1999@gmail.com>
2021-11-02 16:26:54 +0300

    9th seminar is done

--- seminar-09/04-seek/main.c
+++ seminar-09/04-seek/main.c
@@ -1,19 +1,27 @@
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <ctype.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <assert.h>
 
-int main(int argc, char *argv[])
-{
+/*
+ * Берем второй символ в файле, преобразовываем к цифре и инкрементим по модулю 10
+ */
+
+int main(int argc, char *argv[]) {
     assert(argc >= 2);
+
     // O_RDWR - открытие файла на чтение и запись одновременно
-    int fd = open(argv[1], O_RDWR | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+    int fd = open(argv[1],
+                  O_RDWR | O_CREAT,
+                  S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH // 0664
+    );
+    if (-1 == fd) {
+        goto terminate;
+    }
 
     // Перемещаемся на конец файла, получаем позицию конца файла - это размер файла
     int size = lseek(fd, 0, SEEK_END);
-
     printf("File size: %d\n", size);
 
     // если размер меньше 2, то дописываем цифры
@@ -29,13 +37,18 @@ int main(int argc, char *argv[])
     // читаем символ со 2й позиции
     lseek(fd, 1, SEEK_SET);
     char c;
-    read(fd, &c, 1);
-    c = (c < '0' || c > '9') ? '0' : ((c - '0') + 1) % 10 + '0';
+    if (0 > (read(fd, &c, 1))) {
+        perror("Cannot read 2nd byte");
+        goto cleanup;
+    }
+    c = !isdigit(c) ? '0' : ((c - '0') + 1) % 10 + '0';
 
     // записываем символ в 2ю позицию
     lseek(fd, 1, SEEK_SET);
     write(fd, &c, 1);
 
+cleanup:
     close(fd);
+terminate:
     return 0;
 }
