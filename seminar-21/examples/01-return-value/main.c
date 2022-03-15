c2cb98b4acfca2addea1a2e7532004b1447acf75
MVCionOld <tsionmichel1999@gmail.com>
2022-03-15 07:10:29 +0300

    21st seminar is done

---
+++ seminar-21/examples/01-return-value/main.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/syscall.h>
+#include <sys/time.h>
+#include <stdint.h>
+#include <string.h>
+#include <pthread.h>
+#include <errno.h>
+
+// log_printf - макрос для отладочного вывода, добавляющий время со старта программы, имя функции и номер строки
+uint64_t start_time_msec; void  __attribute__ ((constructor)) start_time_setter() { struct timespec spec; clock_gettime(CLOCK_MONOTONIC, &spec); start_time_msec = spec.tv_sec * 1000L + spec.tv_nsec / 1000000; }
+const char* log_prefix(const char* func, int line) {
+    struct timespec spec; clock_gettime(CLOCK_MONOTONIC, &spec); int delta_msec = spec.tv_sec * 1000L + spec.tv_nsec / 1000000 - start_time_msec;
+    const int max_func_len = 13; static __thread char prefix[100];
+    sprintf(prefix, "%d.%03d %*s():%-3d [tid=%ld]", delta_msec / 1000, delta_msec % 1000, max_func_len, func, line,
+            (long)pthread_self());
+    return prefix;
+}
+#define log_printf_impl(fmt, ...) { time_t t = time(0); dprintf(2, "%s: " fmt "%s", log_prefix(__FUNCTION__, __LINE__), __VA_ARGS__); }
+// Format: <time_since_start> <func_name>:<line> : <custom_message>
+#define log_printf(...) log_printf_impl(__VA_ARGS__, "")
+
+
+#define fail_with_strerror(code, msg) do { char err_buf[1024]; strerror_r(code, err_buf, sizeof(err_buf));\
+    log_printf(msg " (From err code: %s)\n", err_buf);  exit(EXIT_FAILURE);} while (0)
+
+// thread-aware assert
+#define ta_verify(stmt) do { if (stmt) break; fail_with_strerror(errno, "'" #stmt "' failed."); } while (0)
+
+// verify pthread call
+#define pt_verify(pthread_call) do { int code = (pthread_call); if (code == 0) break; \
+    fail_with_strerror(code, "'" #pthread_call "' failed."); } while (0)
+
+
+typedef struct {
+  int a;
+  int b;
+} thread_task_args_t;
+
+// На самом деле проще записать результат в структуру аргументов
+typedef struct {
+  int c;
+} thread_task_result_t;
+
+static thread_task_result_t*
+thread_func(const thread_task_args_t* arg) {
+    log_printf("\tThread func started\n");
+    thread_task_result_t* result = (thread_task_result_t*)malloc(sizeof(thread_task_result_t));
+    ta_verify(result != NULL);
+    result->c = arg->a + arg->b;
+    log_printf("\tThread func finished\n");
+    return result;
+}
+
+int main() {
+    log_printf("Main func started\n");
+    pthread_t thread;
+
+    thread_task_args_t args = {.a = 35, .b = 7};
+    log_printf("Thread creating, args are: a=%d b=%d\n", args.a, args.b);
+    pt_verify(pthread_create(
+        &thread, NULL,
+        (void* (*)(void*))thread_func, // Важно понимать, что тут происходит
+        (void*)&args
+    ));
+
+    thread_task_result_t* result;
+    pt_verify(pthread_join(thread, (void**)&result));
+    log_printf("Thread joined. Result: c=%d\n", result->c);
+    free(result);
+
+    log_printf("Main func finished\n");
+    return 0;
+}
